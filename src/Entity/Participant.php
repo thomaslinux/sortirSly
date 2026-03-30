505d920081a47fe2d80e9820faf2c1511465ed6e
chuet2025 <clara.huet2025@campus-eni.fr>
2026-03-30 14:47:55 +0200

    changement au getRole

--- src/Entity/Participant.php
+++ src/Entity/Participant.php
@@ -118,9 +118,9 @@ class Participant implements UserInterface, PasswordAuthenticatedUserInterface
      */
     public function getRoles(): array
     {
-//        $roles = $this->username;
+        $roles = $this->roles;
         // guarantee every user at least has ROLE_USER
-        $roles = ['ROLE_USER'];
+        $roles[] = 'ROLE_USER';
 
         return array_unique($roles);
     }
