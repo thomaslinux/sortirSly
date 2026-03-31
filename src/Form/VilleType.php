2cfa05a89ddab4623aa2c416106b1432f67ed3c6
dthebault2025 <david.thebault2025@campus-eni.fr>
2026-03-31 14:00:52 +0200

    GEstion des villes (ajout) ok reste modifier et supprimer

--- src/Form/VilleType.php
+++ src/Form/VilleType.php
@@ -2,27 +2,34 @@
 
 namespace App\Form;
 
-use App\Form\Model\VilleSearch;
+use App\Entity\Ville;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class VilleSearchType extends AbstractType
+class VilleType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom',TextType::class, [
-        'label' => 'Ville :'
-    ]);
+            ->add('nom', TextType::class, [
+                'label' => 'Nouvelle ville :'
+            ])
+            ->add('codePostal', IntegerType::class, [
+                'label' => 'Code postal associé :',
+                'required' => false,
+            ]);
 
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'data_class' => VilleSearch::class,
+            'data_class' => Ville::class,
+            'required'=>false
         ]);
     }
 }
