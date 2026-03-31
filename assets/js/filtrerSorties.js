0aa89c37edee0f1229a6981e8b9ae9948bf27db0
thomaslinux <182340067+thomaslinux@users.noreply.github.com>
2026-03-31 10:22:45 +0200

    Ajout d'un bouton pour effacer le champ de recherche et d'une checkbox pour désactiver la recherche javascript
    Signed-off-by: thomaslinux <182340067+thomaslinux@users.noreply.github.com>

--- assets/js/filtrerSorties.js
+++ assets/js/filtrerSorties.js
@@ -1,19 +1,46 @@
 const searchInput = document.getElementById('sortie_search_nom');
+const client_search = document.getElementById('client_search');
+const HTML_clear_btn = document.getElementById('clear-btn');
 const HTML_nbSortiesTrouvees = document.getElementById('nbSortiesTrouvees');
 
 function filterSorties() {
-    const query = searchInput.value.trim().toLowerCase();
-    let i = 0;
-    document.querySelectorAll('tbody .event-name a').forEach(a => {
-        const tr = a.closest('tr');
-        if (query === '' || a.innerText.trim().toLowerCase().includes(query)) {
+    if (searchInput.value.length > 0) {
+        console.log("hello");
+        HTML_clear_btn.style.display = '';
+    }
+    if (client_search.checked) {
+        const query = searchInput.value.trim().toLowerCase();
+        let i = 0;
+        document.querySelectorAll('tbody .event-name a').forEach(a => {
+            const tr = a.closest('tr');
+            if (query === '' || a.innerText.trim().toLowerCase().includes(query)) {
+                tr.style.display = '';
+                i++;
+                HTML_nbSortiesTrouvees.innerText = i;
+            } else {
+                tr.style.display = 'none';
+            }
+        });
+    }
+}
+
+client_search.addEventListener('click', () => {
+    if (!client_search.checked) {
+        document.querySelectorAll("tr").forEach(tr => {
             tr.style.display = '';
-            i++;
-            HTML_nbSortiesTrouvees.innerText = i;
-        } else {
-            tr.style.display = 'none';
-        }
-    });
+        })
+        clearInput();
+    } else {
+        filterSorties();
+    }
+})
+
+HTML_clear_btn.addEventListener('click', clearInput)
+
+function clearInput() {
+    searchInput.value = '';
+    HTML_clear_btn.style.display = 'none';
+    filterSorties();
 }
 
 searchInput.addEventListener('keyup', filterSorties);
