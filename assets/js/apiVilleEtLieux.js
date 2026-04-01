185638b9d51b04bb0a8dccfa8b4782bed1d082b3
dthebault2025 <david.thebault2025@campus-eni.fr>
2026-04-01 15:33:15 +0200

    Modif template creation et update de sortie

--- assets/js/apiVilleEtLieux.js
+++ assets/js/apiVilleEtLieux.js
@@ -17,6 +17,7 @@ async function callAPI(url) {
 
 function init() {
     showHide();
+    modifLieux();
     displayLieu();
 }
 
@@ -55,18 +56,21 @@ function inverse() {
     lieu2.classList.toggle('hide-me');
 }
 
+function modifLieux(){
+    document.getElementById('mask').addEventListener("click", (e) => {
+        e.preventDefault();
+        inverse()
+        selectLieu.innerHTML = "";
+        selectVille.value = "";
+        selectLieu.innerHTML = '<option value="" selected hidden>- Lieu de sortie -</option>';
 
-document.getElementById('mask').addEventListener("click", () => {
-    inverse()
-    selectLieu.innerHTML = "";
-    selectVille.value = '';
-    selectLieu.innerHTML = '<option value="" selected hidden>- Lieu de sortie -</option>';
-
-    if (document.getElementById('mask').innerHTML === "Ajouter un lieu") {
-        document.getElementById('mask').innerHTML = "Revenir au choix par défaut"
-    } else {
-        document.getElementById('mask').innerHTML = "Ajouter un lieu"
-    }
+        if (document.getElementById('mask').textContent.trim() === "Ajouter un lieu") {
+            document.getElementById('mask').innerHTML = "Revenir au choix par défaut"
+        } else {
+            document.getElementById('mask').innerHTML = "Ajouter un lieu"
+        }
+    })
 
 
-})
+
+}
